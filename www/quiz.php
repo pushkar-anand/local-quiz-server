a9ee00fac82fb0a49b9a44fb7cff1f5fe243ed53
Pushkar Anand <anandpushkar088@gmail.com>
2017-10-18 17:42:07 +0530

    started working on quiz file

---
+++ www/quiz.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../includes/session-functions.php';
+secure_session('quiz_user');
+require_once '../includes/site-config.php';
+require_once '../includes/functions.php';
+
+if (isset($_GET['user']))
+{
+  if ( isset($_SESSION['name']) && isset($_SESSION['email']) )
+  {
+    $user = test_input($_GET['user']);
+    $name = test_input($_SESSION['name']);
+    $email = test_input($_SESSION['email']);
+
+  }
+  else
+  {
+    header("HTTP/1.0 400 Bad Request");
+  }
+}
+else
+{
+  header("HTTP/1.0 400 Bad Request");
+}
+
+?>
